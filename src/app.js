3c45cf59c641cf33c999ed24969caf3b89c97895
Mar Aguayo <mar.aguayo15@gmail.com>
2024-01-23 14:26:28 +0000

    First commit

--- src/app.js
+++ src/app.js
@@ -5,7 +5,52 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
+window.onload = () => {
+  document.querySelector("#boton").addEventListener("click", () => {
+    document.querySelector("#excusa").innerHTML = resultado();
+  });
   //write your code here
-  console.log("Hello Rigo from the console!");
+
+  let resultado = () => {
+    let quien = [
+      "Mi vecino",
+      "Tu padre",
+      "El presidente",
+      "Un elefante",
+      "El portero",
+      "Roberta"
+    ];
+    let que = ["quemó", "vendió", "estrelló", "se comió", "pintó", "voló"];
+    let evitar = [
+      "las ventanas",
+      "los trabajos",
+      "el proyecto",
+      "mi código",
+      "todo mi sueldo",
+      "mi alma"
+    ];
+    let cuando = [
+      "el 3 de enero",
+      "mientras dormía",
+      "durante la cena",
+      "cuando estaba en el ascensor",
+      "ayer",
+      "hace 10 años"
+    ];
+
+    let aleatorioquien = Math.floor(Math.random() * quien.length);
+    let aleatorioque = Math.floor(Math.random() * que.length);
+    let aleatorioevitar = Math.floor(Math.random() * evitar.length);
+    let aleatoriocuando = Math.floor(Math.random() * cuando.length);
+
+    return (
+      quien[aleatorioquien] +
+      " " +
+      que[aleatorioque] +
+      " " +
+      evitar[aleatorioevitar] +
+      " " +
+      cuando[aleatoriocuando]
+    );
+  };
 };
